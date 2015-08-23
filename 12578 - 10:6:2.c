412e4aa3ede8ad85020aadcba20ed9a41b94f7df
Kawsar Ahmed <mkawsar10@gmail.com>
2015-08-24 01:37:28 +0600

    Update 12578 - 10:6:2.c

--- 12578 - 10:6:2.c
+++ 12578 - 10:6:2.c
@@ -2,25 +2,30 @@
 Problem: 12578 - 10:6:2
 */
 #include<stdio.h>
- 
+
 #define PI acos(-1)
- 
- 
+
+
 int main()
 {
-	int L, t, i;
-	double arr, div;
- 
+	int t, i;
+	double L, w, r;
+	double ac, div;
+
 	scanf("%d", &t);
 	for(i=0; i<t; i++){
-		scanf("%d", &L);
- 
-		arr= (125663.71*L*L)/1000000;
-		div= (474336.29*L*L)/1000000;
-		printf("%.2f %.2f\n", arr, div );
- 
+		scanf("%lf", &L);
+	w = 6*L/10;
+	r= L/5;
+	ac= PI*r*r;
+	/* My concept was
+	ac= (125663.71*L*L)/1000000;
+	di= (474336.29*L*L)/1000000;
+	*/
+		printf("%.2f %.2f\n", ac, w*L-ac );
+
 	}
- 
- 
+
+
     return 0;
 }
