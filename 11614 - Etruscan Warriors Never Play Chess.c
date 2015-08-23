79814acab591167f811902c991333b9b1ac70633
Kawsar Ahmed <mkawsar10@gmail.com>
2015-08-23 20:27:29 +0600

    Etruscan Warriors Never Play Chess
    By Me. noT accepteD

---
+++ 11614 - Etruscan Warriors Never Play Chess.c
@@ -0,0 +1,36 @@
+/* Date: 18.08.15
+Problem: 11614 - Etruscan Warriors Never Play Chess
+*/
+#include<stdio.h>
+
+int main()
+{
+	long long input;
+	int i=0, x, t;
+	long long maxi, n;
+
+	scanf("%d", &t);
+
+	for(i=0; i<t; i++){
+		maxi = 1;
+		n = 0;
+		scanf("%lld", &input);
+		for(x=2; ; x++){
+			if(input==0){
+				n=0;
+				break;
+			}
+			if(input<3){
+				n=1;
+				break;
+			}
+			if(maxi>input){
+				break;
+			}
+			maxi= maxi+x;
+			n++;
+		} printf("%lld\n", n);
+	}
+
+return 0;
+}
