a78648c84bfac85ed2635e1d2bf451e19c3a6178
Kawsar Ahmed <mkawsar10@gmail.com>
2015-08-28 11:59:34 +0600

    A Minimum Land Price

---
+++ 11824 - A Minimum Land Price.c
@@ -0,0 +1,46 @@
+/* Date: 14.08.15
+Problem : 11824 - A Minimum Land Price
+*/
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+
+int main()
+{
+	int pric[40];
+	long total;
+	int i, n, t, j, s;
+
+	scanf("%d", &t);
+	while(t--){
+		s= 0;
+		total= 0;
+		for(i=0; ; i++){
+			scanf("%d", &pric[i]);
+			if(pric[i]==0) break;
+			s= i;
+		}
+
+        for(i=0; i<s+1; i++){
+			for(j=i+1; j<s+1; j++){
+				if(pric[i]>pric[j]){
+					int temp= pric[i];
+					pric[i]= pric[j];
+					pric[j]= temp;
+				}
+			}
+        }
+	int x= 1;
+	
+        for(i=s; i>=0; i--){
+			total= total+ (2*pow(pric[i], x));
+			x++;
+        }
+        if(total>5000000){
+			printf("Too expensive\n");
+        }
+		else printf("%ld\n", total);
+	}
+
+	return 0;
+}
