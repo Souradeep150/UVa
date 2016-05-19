e34b2916b69589c215c22cc0b386b48621d2abbd
Kawsar Ahmed <mkawsar10@gmail.com>
2016-05-19 21:51:52 +0600

    Create 579 - Clock Hands.cpp

---
+++ 579 - Clock Hands.cpp
@@ -0,0 +1,35 @@
+/* Problem: 579 - Clock Hands
+*/
+#include<bits/stdc++.h>
+#include<cstdio>
+
+using namespace std;
+
+int main(){
+
+    double m,hour;
+    double angle,hourAng,minuteAng;
+    char dot;
+
+    while(1){
+
+        cin >> hour >> dot >> m;
+
+        if(hour==0 && m==0) break;
+
+        hourAng=hour*30+(m/60)*30;
+        minuteAng=m*6;
+
+        angle=(hourAng-minuteAng);
+
+        if(angle<0){
+            angle*=-1;
+        }
+        if (angle>180){
+            angle=360-angle;
+        }
+        printf("%.3f\n",angle);
+    }
+
+    return 0;
+}
